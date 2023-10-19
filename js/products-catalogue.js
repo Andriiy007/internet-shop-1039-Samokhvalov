1f5576b6baf3fcc41e374f817f3f3daa136ee19d
SamikQ <samokhvalov.dmitriy@gmail.com>
2023-10-20 00:38:38 +0300

    catalogue created. Search panel created

--- js/products-catalogue.js
+++ js/products-catalogue.js
@@ -1,7 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-const catalogue = document.querySelector('.catalogue');
+const search = document.querySelector('.products-catalogue__search-input');
+const tableRows = document.querySelectorAll('tbody tr');
 
-function renderCatalogueList() {
-    const table = document.createElement()
+search.addEventListener('input', searchTable);
+
+function searchTable() {
+    tableRows.forEach((row, i) => {
+        let catalogueData = row.textContent.toLocaleLowerCase();
+        let searchData = search.value.toLocaleLowerCase();
+        row.classList.toggle('hidden', catalogueData.indexOf(searchData) < 0);
+        row.style.setProperty('--delay', i / 25 + 's');
+        setTimeout(() => {
+            if (row.classList.contains('hidden')) {
+                row.remove();
+            }
+        }, 1000);
+    });
 }
-});
+
+const sortBtns = document.querySelectorAll('.table__item-title');
+
+function sortBy(e) {
+    sortBtns.forEach(item => {
+        if ();
+    })
+}
+
